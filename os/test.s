f9dd7d5d82e81703e66a0368247d2fa8de9d7521
luckiezhou <luckiezhou@gmail.com>
2012-11-25 19:51:53 +0800

    MODIFIED: modifications corresponding to the change of mem model

--- os/test.s
+++ os/test.s
@@ -4,19 +4,43 @@
 .inc "syscall.inc"
 .inc "io.lib"
 
-.offset 0x800000
+.offset 0x1c00000 #28M
 
 .static {
-CUR_USR_PROC:
+CUR_PROC:
     .0
-PROC_SEGBASE:    #starting from system
-    .dup 32 .0
-PROC_CURSP:      #starting from system
-    .dup 32 .0
+PROC_INFO:
+    #8word: gdt, 32word: gpr, 1word: epc, 1word: hi, 1word: lo, 5word: reserved
+    #proc_info for init
+    .0x0        #physical: 0
+    .0x0        #virtual: 0
+    .0x1800000  #size: 24M
+    .0          #reserved
+    .0x0        #physical: 0
+    .0x1800000  #virtual: 24M
+    .0x800000   #size: 8M
+    .0          #reserved
+    .dup 40 .0
+
+    .dup 48 * 63 .0
 }
 
+BOOT:
+    #calc physical address for proc_info
+    lli $t0, 0x1800000
+    lla $t1, PROC_INFO
+    sub $t1, $t1, $t0
+    #move inst from real BT1 to virtual BT1
+    lla $t2, BT1
+    sub $t3, $t2, $t0
+    lw $t3, 0($t3)
+    sw $t3, 0($t2)
+    j BT1
+BT1:
+    mtc0 $t1, $0
+    
 INIT:
-    ori $sp, $zero, 0x8000      #j
+    lli $sp, OS_STACK_INIT      #j
     lla $t0, WTF
     srl $t0, $t0, 2
     lui $t1, 0x0800
