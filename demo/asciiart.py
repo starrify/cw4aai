b23959efa7e0450194c036166b76b24c4d57f2da
Pengyu CHEN <cpy.prefers.you@gmail.com>
2012-12-10 02:22:50 +0800

    FIXED: again.. fixed asciiart.py with python2 and python3

--- demo/asciiart.py
+++ demo/asciiart.py
@@ -1,17 +1,22 @@
+import sys
+
+def _input():
+    if sys.version_info[0] == 2:
+        return raw_input()
+    else:
+        return input()
+
 def main():
-    try: # python3 would do
-        s = input().replace("\n","")
-    except:
-        s = raw_input().replace("\n","")
+    s = _input().replace("\n","")
     for i in s.split(" "):
         print(".0x%X" %int(i))
     nframe = int(s.split(" ")[2])
     nline = int(s.split(" ")[1])
     for iframe in range(nframe):
         print("# frame %d" %iframe)
-        s = input() # skip line
+        s = _input() # skip line
         for iline in range(nline):
-            s = input()
+            s = _input()
             for c in s:
                 print(".0x%X" %ord(c))
             print(".0x0A")
